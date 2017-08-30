fe778bd7e564ba7504193e6fa72dffdc50a23583
wangyinghua <itwyhuaing@163.com>
2017-08-31 07:39:32 +0800

    笔记补充

---
+++ SwiftProDemoOne/SwiftProDemoOne/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  SwiftProDemoOne
+//
+//  Created by wangyinghua on 2017/8/30.
+//  Copyright © 2017年 wangyinghua. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow.init(frame:UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        let firstVC = FirstVC()
+        let secondVC = SecondVC()
+        let firstNav = UINavigationController.init(rootViewController: firstVC)
+        let secondNav = UINavigationController.init(rootViewController: secondVC)
+        let tabBarVC = UITabBarController.init()
+        tabBarVC.viewControllers = [firstNav,secondNav]
+        window?.rootViewController = tabBarVC
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ SwiftProDemoOne/SwiftProDemoOne/SecondVC.swift
@@ -0,0 +1,37 @@
+
+
+//
+//  SecondVC.swift
+//  SwiftProDemoOne
+//
+//  Created by wangyinghua on 2017/8/30.
+//  Copyright © 2017年 wangyinghua. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "SecondVC"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
