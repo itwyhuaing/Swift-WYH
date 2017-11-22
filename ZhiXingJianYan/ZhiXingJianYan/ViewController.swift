9cff9fd7eb8533026333864d66610486b8579ec2
wangyinghua <itwyhuaing@163.com>
2017-11-22 23:16:29 +0800

    Tab 框架导入

---
+++ ZhiXingJianYan/ZhiXingJianYan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiXingJianYan
+//
+//  Created by wangyinghua on 2017/11/22.
+//  Copyright © 2017年 ZhiXing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
