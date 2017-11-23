1670436dfc5f19e714c125ebeb6666c8f82e5659
hnbwyh <rainwang@hinabian.com>
2017-11-23 19:20:06 +0800

    OC于Swift混编

---
+++ ZhiXingJianYan/ZhiXingJianYan/Modules/MainTabBarVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarVC.swift
+//  ZhiXingJianYan
+//
+//  Created by hnbwyh on 2017/11/23.
+//  Copyright © 2017年 ZhiXing. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarVC: CYLTabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ZhiXingJianYan/ZhiXingJianYan/ZhiXingJianYan-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  ZhiXingJianYan-Bridging-Header.h
+//  ZhiXingJianYan
+//
+//  Created by hnbwyh on 2017/11/23.
+//  Copyright © 2017年 ZhiXing. All rights reserved.
+//
+
+#ifndef ZhiXingJianYan_Bridging_Header_h
+#define ZhiXingJianYan_Bridging_Header_h
+
+//新增CYLTabBar
+#import "CYLTabBarController.h"
+#import "CYLTabBar.h"
+#import "CYLPlusButton.h"
+
+#endif /* ZhiXingJianYan_Bridging_Header_h */
